b16265c03cf51005d28f280a278349abdde3d311
Christofer Berruz <cberruzc@my.bridgeport.edu>
2019-01-06 19:20:27 -0500

    Main script for the GEVP for matrix C(t)_i as a function of i = (nsamp, nproc)

---
+++ generalized_eigenvalue.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy import linalg as la
+import h5py
+import math
+import jackknife_ITCM as jk
+import printingmethods as printm
+
+def getStructures_fromH5File(hdf5_filename, key):
+	with h5py.File(hdf5_filename, "r") as f:
+		try:
+			t = f.get(key).value
+			print("Succefully returned structure")
+			return t
+		except Exception as e:
+			print("Error at getStructures_fromH5File: " + str(e))
+
+def GEVP(ITCM, SIGN, to):
+	ITCM_JK = jk.jackknife_ITCM(ITCM, SIGN)
+	nsamp, nproc, nt, ni, nj = ITCM_JK.shape
+	Eigenvalues = np.zeros((nsamp, nproc, nt, ni), dtype=np.float64)
+	#Calculating eigenvalues per samp, proc, t > to.
+	for samp in range(nsamp):
+		for proc in range(nproc):
+			for t in range(to+1,nt):
+				B = symmetricMatrix(np.array(ITCM_JK[samp, proc, to], dtype=np.float64))
+				A = symmetricMatrix(np.array(ITCM_JK[samp,proc,t], dtype=np.float64))
+				Eigenvalues[samp, proc, t], _ = la.eigh(A, B, lower=False)
+	#Averaging eigenvalues over nsamp, nproc
+	Eigenvalues_averaged = np.mean(Eigenvalues, axis=(0,1), dtype=np.float64)
+	#Calculating jackknife errors for t > to
+	Eigenvalues_errors = np.zeros((nt, ni), dtype=np.float64)
+	for t in range(to+1, nt):
+		dummy = np.zeros(ni, dtype=np.float64)
+		for samp in range(nproc):
+			for proc in range(nproc):
+				dummy = np.add(dummy, (Eigenvalues[samp, proc, t]-Eigenvalues_averaged[t])**2)
+		Eigenvalues_errors[t] = np.true_divide(np.multiply(nsamp*nproc-1, dummy), (nsamp*nproc))
+		Eigenvalues_errors[t] = np.sqrt(Eigenvalues_errors[t])
+	return np.array([Eigenvalues_averaged[:, :],Eigenvalues_errors[:,:]],dtype=np.float64)
+
+def symmetricMatrix(A):
+	'''
+	:param: A is a 2-D matrix
+	:return: symmetric version of A copying the upper triangular matrix
+	'''
+	ni, nj = A.shape
+	l = 1
+	A_symm = np.triu(A)
+	for i in range(ni):
+		for j in range(l, nj):
+			A_symm[j,i] = A_symm[i,j]
+		l += 1
+	return A_symm
+
+def main():
+	filename = "10Be.db32.nb016.h5.A"
+	ITCM = getStructures_fromH5File(filename, 'C(K=2,pi=+)')
+	SIGN = getStructures_fromH5File(filename, 'sign')
+	to = 0
+	Eigvals_and_errors = GEVP(ITCM, SIGN, to)
+	outfile_gevp = filename + ".GEVP.txt"
+	outfile_itcm = filename + ".ITCM.txt"
+	printm.printGEVP_withErrors(Eigvals_and_errors, to, outfile_gevp)
+	printm.printITCM(outfile_itcm,ITCM,avg=False)
+
+
+if __name__ == '__main__':main()
